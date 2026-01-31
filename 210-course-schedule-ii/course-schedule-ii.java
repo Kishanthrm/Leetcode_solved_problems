74f924d7b9861af93e0e4c00fd67af009ea0a4b4
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-01-31 10:04:38 +0530

    Time: 6 ms (50.57%) | Memory: 46.7 MB (79.29%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int n, int[][] mat) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        int[] indeg = new int[n];
+        for (int[] edge : mat) {
+            int ai = edge[0];
+            int bi = edge[1];
+            adj.get(bi).add(ai);
+            indeg[ai]++;
+        }
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0) {
+                que.offer(i);
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (que.size() > 0) {
+            int node = que.poll();
+            ans.add(node);
+            for (int a : adj.get(node)) {
+                indeg[a]--;
+                if (indeg[a] == 0) {
+                    que.offer(a);
+                }
+            }
+        }
+        if(ans.size() != n)return new int[0];
+        int[] arr = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            arr[i] = ans.get(i); 
+        }
+        return arr;
+    }
+}
