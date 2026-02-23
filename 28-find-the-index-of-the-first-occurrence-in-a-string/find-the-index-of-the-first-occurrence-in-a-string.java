7a994ce47f7f58d694ca49eb353259316d13a280
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-02-23 17:53:52 +0530

    Time: 4 ms (6.48%) | Memory: 43.1 MB (69.73%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,39 +1,48 @@
 class Solution {
-    public int[] find(String s){
-        int lps[]=new int [s.length()];
-        int len=0,i=1;
-        while(i<s.length()){
-            if(s.charAt(i)==s.charAt(len)){
+
+    public int[] findLPS(String s) {
+        int[] lps = new int[s.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < s.length()) {
+            if (s.charAt(i) == s.charAt(len)) {
                 len++;
-                lps[i]=len;
+                lps[i] = len;
                 i++;
-            }
-            else if(s.charAt(i)!=s.charAt(len)){
-                if(len!=0){
-                    len=lps[len-1];
-                }
-                else{
-                    lps[i]=0;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
                     i++;
                 }
             }
         }
         return lps;
     }
+
     public int strStr(String haystack, String needle) {
-        int lps[]=find(needle);
-        int j=0,i=0;
+        if (needle.length() == 0) return 0;
+
+        int[] lps = findLPS(needle);
+
+        int i = 0; // haystack pointer
+        int j = 0; // needle pointer
+
         while (i < haystack.length()) {
+
             if (haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
             }
+
             if (j == needle.length()) {
-                return i - j;
+                return i - j;  // match found
             } 
             else if (i < haystack.length() && haystack.charAt(i) != needle.charAt(j)) {
                 if (j != 0) {
-                    j = lps[j - 1];
+                    j = lps[j - 1];  // jump using LPS
                 } else {
                     i++;
                 }
