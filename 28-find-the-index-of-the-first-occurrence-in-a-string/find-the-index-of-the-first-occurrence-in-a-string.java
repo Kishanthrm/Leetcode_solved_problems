8bde4b5227a8c7f23c566ded4fab801d03fe1143
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-02-23 17:53:15 +0530

    Time: 4 ms (6.48%) | Memory: 43.2 MB (34.71%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,23 +1,44 @@
 class Solution {
-    public int strStr(String text, String pat) {
-        if(text.length() == 1 && pat.length() == 1 && text.charAt(0) == pat.charAt(0))return 0;
-        int n = text.length(), m = pat.length();
-        int ans = -1;
-        boolean flag = false;
-        for(int i=0;i<n-m+1;i++){
-            for(int j=0;j<m;j++){
-                if(text.charAt(i+j) != pat.charAt(j)){
-                    break;
-                }
-                if(j == m-1){
-                    ans = i;
-                    flag = true;
-                }
+    public int[] find(String s){
+        int lps[]=new int [s.length()];
+        int len=0,i=1;
+        while(i<s.length()){
+            if(s.charAt(i)==s.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
             }
-            if(flag){
-                break;
+            else if(s.charAt(i)!=s.charAt(len)){
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
             }
         }
-        return ans;
+        return lps;
+    }
+    public int strStr(String haystack, String needle) {
+        int lps[]=find(needle);
+        int j=0,i=0;
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == needle.length()) {
+                return i - j;
+            } 
+            else if (i < haystack.length() && haystack.charAt(i) != needle.charAt(j)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return -1;
     }
 }
