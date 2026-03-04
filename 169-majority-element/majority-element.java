9897cbd6664151705211da7ae48bc0776da2a8de
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-04 22:50:03 +0530

    Time: 6 ms (48.61%) | Memory: 55.6 MB (62.28%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        } else {
+            Arrays.sort(nums);
+            
+                return nums[nums.length / 2];
+            
+        }
+    }
+}
