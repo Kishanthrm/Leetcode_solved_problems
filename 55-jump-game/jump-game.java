384f253a244c497263a250c1c4bb21a871ba72c4
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-05 18:33:44 +0530

    Time: 2 ms (85.79%) | Memory: 48 MB (61.90%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(i > max) return false;
             max = Math.max(max,nums[i]+i);
+            if(max>=nums.length)return true;
         }
         return true;
     }
