226646f2fea91e94d149fb9a3059e234be10d9a3
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-07 09:38:19 +0530

    Time: 2 ms (91.06%) | Memory: 43.8 MB (95.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,50 +1,48 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int fresh = 0, m = grid.length, n = grid[0].length,l = 0;
+        int[][] visited = new int[m][n];
         Queue<Pair> que = new LinkedList<>();
-        int[][] vis = new int[grid.length][grid[0].length];
-        int[] ro = { -1, 0, 1, 0 };
-        int[] co = { 0, 1, 0, -1 };
-        int l = 0, fresh = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
-                    que.offer(new Pair(i, j, l));
-                } else if (grid[i][j] == 1) {
-                    ++fresh;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 2){
+                    que.offer(new Pair(i,j,l));
+                }
+                if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
-        while (que.size() > 0) {
-
-            int row, col;
+        int[] ro = {-1,0,1,0};
+        int[] co = {0,1,0,-1};
+        while(!que.isEmpty()){
             Pair node = que.poll();
+            int r = node.r;
+            int c = node.c;
             l = node.t;
-            int r = node.i;
-            int c = node.j;
-            for (int d = 0; d < 4; d++) {
-                row = r + ro[d];
-                col = c + co[d];
-                if ((row >= 0 && row <= grid.length - 1) && (col >= 0 && col <= grid[0].length - 1)
-                        && vis[row][col] == 0 && grid[row][col] == 1) {
-                    vis[row][col] = 1;
+            for(int d=0;d<4;d++){
+                int row = r + ro[d];
+                int col = c + co[d];
+                if(((row >= 0 && col >= 0) && (row < m && col < n)) && (visited[row][col] == 0) && (grid[row][col] == 1)){
                     grid[row][col] = 2;
-                    que.offer(new Pair(row, col, l + 1));
+                    visited[row][col] = 1;
+                    que.offer(new Pair(row,col,l+1));
                     --fresh;
                 }
             }
         }
-        return fresh == 0 ? l : -1;
+        return fresh == 0? l : -1;
     }
 }
 
-class Pair {
-    int i;
-    int j;
+class Pair{
+    int r;
+    int c;
     int t;
 
-    Pair(int i, int j, int t) {
-        this.i = i;
-        this.j = j;
+    Pair(int r, int c, int t){
+        this.r = r;
+        this.c = c;
         this.t = t;
     }
 }
