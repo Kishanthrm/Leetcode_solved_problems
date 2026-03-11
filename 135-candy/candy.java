e1dc9e3e123acf6d505073839df9fd30f85ee793
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-11 11:27:06 +0530

    Time: 3 ms (86.02%) | Memory: 47.8 MB (29.50%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,28 +2,26 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] candy = new int[n];
+        
         Arrays.fill(candy,1);
 
-        boolean changed = true;
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candy[i] = candy[i-1] + 1;
+            }
+        }
 
-        while(changed){
-            changed = false;
-
-            for(int i=0;i<n;i++){
-                if(i>0 && ratings[i] > ratings[i-1] && candy[i] <= candy[i-1]){
-                    candy[i] = candy[i-1] + 1;
-                    changed = true;
-                }
-
-                if(i<n-1 && ratings[i] > ratings[i+1] && candy[i] <= candy[i+1]){
-                    candy[i] = candy[i+1] + 1;
-                    changed = true;
-                }
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candy[i] = Math.max(candy[i], candy[i+1] + 1);
             }
         }
 
         int sum = 0;
-        for(int c : candy) sum += c;
+        for(int c : candy){
+            sum += c;
+        }
+
         return sum;
     }
 }
