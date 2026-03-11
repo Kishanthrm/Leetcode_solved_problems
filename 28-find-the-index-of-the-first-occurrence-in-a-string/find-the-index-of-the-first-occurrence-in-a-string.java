f21b4795be0192b2e2a3bffa208b734c46254ec7
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-11 13:43:05 +0530

    Time: 164 ms (5.44%) | Memory: 43.4 MB (17.83%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,52 +1,22 @@
 class Solution {
-
-    public int[] findLPS(String s) {
-        int[] lps = new int[s.length()];
-        int len = 0;
-        int i = 1;
-
-        while (i < s.length()) {
-            if (s.charAt(i) == s.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-        return lps;
-    }
-
-    public int strStr(String haystack, String needle) {
-        if (needle.length() == 0) return 0;
-
-        int[] lps = findLPS(needle);
-
-        int i = 0; // haystack pointer
-        int j = 0; // needle pointer
-
-        while (i < haystack.length()) {
-
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
+    public int strStr(String h, String n) {
+        int hsize = h.length(), nsize = n.length();
+        boolean flag = true;
+        if(hsize < nsize)return -1;
+        int i=0, j=0;
+        while(i < hsize){
+            int temp = i;
+            j = 0;
+            flag =true;
+            while(temp < hsize && flag){
+                if(h.charAt(temp) != n.charAt(j)){
+                    flag = false;
+                }  
+                if(j == nsize-1 && flag)return i;
+                temp++;
                 j++;
             }
-
-            if (j == needle.length()) {
-                return i - j;  // match found
-            } 
-            else if (i < haystack.length() && haystack.charAt(i) != needle.charAt(j)) {
-                if (j != 0) {
-                    j = lps[j - 1];  // jump using LPS
-                } else {
-                    i++;
-                }
-            }
+            i++;
         }
         return -1;
     }
