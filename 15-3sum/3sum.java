ad4f8da71a510a4a758099c70af9e6a6a2e7375a
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-14 09:29:23 +0530

    Time: 32 ms (77.51%) | Memory: 60.4 MB (9.74%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+        int size = nums.length;
         List<List<Integer>> list = new ArrayList<>();
-        for(int start = 0;start<nums.length-2;start++){
-            if (start > 0 && nums[start] == nums[start - 1]) continue;
-            int i = start+1;
-            int j = nums.length-1;
-            while(i<j){
-                if(nums[start] + nums[i]+nums[j] == 0){
-                    List<Integer> inlist = new ArrayList<>();
-                    inlist.add(nums[start]);
-                    inlist.add(nums[i]);
-                    inlist.add(nums[j]);
-                    list.add(inlist);
-                    i++;
-                    j--;
-                    while (i < j && nums[i] == nums[i - 1]) i++; 
-                    while (i < j && nums[j] == nums[j + 1]) j--;
+        Arrays.sort(nums);
+        for(int i=0;i<size;i++){
+            if(i != 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1, k = size-1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    list.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1])j++;
+                    while(j < k && nums[k] == nums[k+1])k--;
                 }
-                else if(nums[start] + nums[i]+nums[j] > 0){
-                    j--;
+                else if(sum > 0){
+                    k--;
                 }
-                else if(nums[start] + nums[i]+nums[j] <0){
-                    i++;
+                else{
+                    j++;
                 }
             }
         }
