8b480cc0d10c32ca233b00b441a035c4e94ebde4
Kishanth RM <145460858+Kishanthrm@users.noreply.github.com>
2026-03-19 10:03:51 +0530

    Time: 22 ms (43.67%) | Memory: 47.4 MB (59.39%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,33 +1,31 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> close = new Stack<>();
-        char[] ch = new char[s.length()];
-        Arrays.fill(ch, '0');
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isLetter(c)) {
-                ch[i] = c;
-            } else {
-                if (c == '(') {
-                    open.push(i);
-                } else {
-                    if (open.isEmpty()) {
-                        close.push(i);
-                    } else {
-                        ch[i] = ')';
-                        ch[open.pop()] = '(';
+        Stack<Integer> st = new Stack<>();
+        char[] arr = new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isLetter(ch)){
+                arr[i] = ch;
+            }
+            else{
+                if(ch == '('){
+                    st.push(i);
+                }
+                else{
+                    if(!st.isEmpty()){
+                        int num = st.pop();
+                        arr[num] = '(';
+                        arr[i] = ')';
                     }
                 }
             }
         }
         StringBuilder str = new StringBuilder();
-        for (char i : ch) {
-            if (i != '0') {
-                str.append(i);
+        for(char ch : arr){
+            if(Character.isLetter(ch) || ch == '(' || ch == ')'){
+                str.append(ch);
             }
         }
         return str.toString();
-
     }
 }
